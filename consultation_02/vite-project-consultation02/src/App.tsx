c86a74dacb1dceb70f630cbcbc8f46fc8afef41a
TetianaAnufriieva <anufrieva.tetiana@gmail.com>
2025-10-18 17:52:32 +0200

    update consultation02

--- consultation_02/vite-project-consultation02/src/App.tsx
+++ consultation_02/vite-project-consultation02/src/App.tsx
@@ -1,12 +1,14 @@
 import type { JSX } from 'react'
 import './App.css'
 import ThemeSwitcher from './components/ThemeSwitcher/ThemeSwitcher'
+import InputMirror from './components/InputMirror/InputMirror'
 
 function App(): JSX.Element {
 
   return (
     <div>
      <ThemeSwitcher />
+     <InputMirror />
     </div>
   )
 }
---
+++ consultation_02/vite-project-consultation02/src/components/InputMirror/InputMirror.tsx
@@ -0,0 +1,21 @@
+import { useState, type JSX } from "react";
+
+export default function InputMirror(): JSX.Element {
+  const [text, setText] = useState<string>("");
+
+function handleChange(event):any {
+    setText(event.target.value);
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        placeholder="Введите текст..."
+        style={{ padding: "5px", fontSize: "48px" }}
+      />
+      <h1>Вы ввели: {text}</h1>
+    </div>
+  );
+}
